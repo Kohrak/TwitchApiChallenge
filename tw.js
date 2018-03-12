c01964cc36905a170f0a1e9382835ac9ec097775
Kohrak <Gabriel@PPM.local>
2018-03-12 11:16:46 +0000

    add display user and status functionality

--- tw.js
+++ tw.js
@@ -5,10 +5,41 @@ function checkOnline(user) {
       xhr.send();
   xhr.onload = function handleRequest() {
     var twtv = JSON.parse(xhr.responseText);
-    if ( twtv.stream !== null){
-      console.log("online");
+    if (twtv.stream !== null){
+      online(user);
     } else {
-      console.log("offline");
+      offline(user);
     }
   }
 }
+
+function online(user){
+  var element = document.getElementById(user);
+  element.textContent += " online";
+  element.setAttribute("class", "online");
+}
+
+function offline(user){
+  var element = document.getElementById(user);
+  element.textContent += " offline";
+  element.setAttribute("class", "offline");
+}
+
+function addUser(user){
+ var element = document.createElement("li");
+ element.setAttribute("id", user);
+ element.innerHTML = user;
+ document.getElementById("userlist").appendChild(element);
+}
+
+var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
+ "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+
+function displayUsers(userNames){
+  for (var i = 0; i < userNames.length; i++){
+    addUser(userNames[i]);
+    checkOnline(userNames[i]);
+  }
+}
+
+displayUsers(userNames);
